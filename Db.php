85e9f8860a1cfc6511f3e5aa34dfbbf6004e2dd1
Matvey Tokarenko <matvey.tokarenko@gmail.com>
2023-05-13 23:27:51 +0300

    refactoring

--- Db.php
+++ Db.php
@@ -2,22 +2,27 @@
 
 class Db
 {
-    protected $chat_id;
-    protected $pdo;
+    protected int $chat_id;
+    protected PDO $pdo;
 
-    public function __construct($chat_id){
+    public function __construct(int $chat_id)
+    {
         $this->chat_id = $chat_id;
         $this->pdo = new PDO('mysql:host=localhost;dbname=arey103_weatherbot','arey103_weatherbot', 'weatherBOT2023');
     }
 
-    public function saveChatId(){
+    public function setChatId(int $chat_id) :self {$this->chat_id = $chat_id; return $this;}
+    public function getChatId():int {return $this->chat_id;}
 
+    public function setPDO(PDO $pdo):self {$this->pdo = $pdo; return $this;}
+    public function getPDO():PDO {return $this->pdo;}
+
+    public function saveChatId():void
+    {
         $query = "SELECT * FROM notes WHERE chat_id = :chat_id";
         $stmt = $this->pdo->prepare($query);
         $stmt->execute([':chat_id'=>$this->chat_id]);
         $res = $stmt->fetchAll();
-
-
 
         if(!$res){
             $query = "INSERT INTO notes SET chat_id=:chat_id";
@@ -26,7 +31,8 @@ class Db
         } 
     }
 
-    public function getNotes(){
+    public function getNotes():string|array
+    {
         $query = "SELECT * FROM notes WHERE chat_id = :chat_id AND note IS NOT NULL";
         $stmt = $this->pdo->prepare($query);
         $stmt->execute([':chat_id'=>$this->chat_id]);
@@ -42,26 +48,29 @@ class Db
         return $message;
     }
 
-    public function setNote($note){
+    public function setNote(string $note):void
+    {
         $query = 'INSERT INTO notes (chat_id, note) VALUES (:chat_id, :note)';
         $stmt = $this->pdo->prepare($query);
         $stmt->execute([':note'=>$note, ':chat_id'=>$this->chat_id]);
     }
 
-    public function deleteNote($id){
+    public function deleteNote(int $id):void
+    {
         $query = "DELETE FROM notes WHERE id = :id AND chat_id = :chat_id";
         $stmt = $this->pdo->prepare($query);
         $stmt->execute([':id'=>$id, ':chat_id'=>$this->chat_id]);
     }
 
-    public function setReminder($id, $unixtime){
+    public function setReminder($id, $unixtime):void
+    {
         $query = "UPDATE notes SET unixtime = :unixtime WHERE id = :id AND chat_id = :chat_id ";
         $stmt = $this->pdo->prepare($query);
         $stmt->execute([':id'=>$id, ':chat_id'=>$this->chat_id, ':unixtime'=>$unixtime]);
-
     }
 
-    public function getLike($substring){
+    public function getLike(string $substring):string|array
+    {
         $note = "%" . $substring . "%";
         $query = "SELECT * FROM notes WHERE chat_id = :chat_id AND note LIKE :note";
         $stmt = $this->pdo->prepare($query);
@@ -78,14 +87,16 @@ class Db
         return $message;
     }
 
-    public function deleteReminder($id){
+    public function deleteReminder(int $id):void
+    {
         $unixtime = NULL;
         $query = "UPDATE notes SET unixtime =:unixtime WHERE id=:id AND chat_id = :chat_id";
         $stmt = $this->pdo->prepare($query);
         $stmt->execute([':id'=>$id, ':chat_id'=>$this->chat_id, ':unixtime'=>$unixtime]);
     }
 
-    public function getBetween($unixtime1, $unixtime2){
+    public function getBetween(int $unixtime1, int $unixtime2):string
+    {
         $query = "SELECT * FROM notes WHERE chat_id = :chat_id AND unixtime BETWEEN :unixtime1 AND :unixtime2";
         $stmt = $this->pdo->prepare($query);
         $stmt->execute([':chat_id'=>$this->chat_id, ':unixtime1'=>$unixtime1, ':unixtime2'=> $unixtime2]);
@@ -94,6 +105,5 @@ class Db
                 $message .= 'ID: ' . $row->id . '; Note: ' . $row->note .  '; Remind: '. date('Y-m-d H:i:s', $row->unixtime) ."\n";
             }
         return $message;
-
     }
 }
--- Telegram.php
+++ Telegram.php
@@ -3,48 +3,51 @@
 
 class Telegram
 {
+    const DICTIONARY = array(
+        'not_listening'=>'',
+        'set_note'=>'Enter your note message',
+        'delete_note'=>'Enter note id to delete it',
+        'delete_remind'=>'Enter note id to delete its remind',
+        'set_remind'=>'Enter note id to edit',
+        'search_note'=>'Enter string to search among notes',
+        'set_between'=>'Set first time like following example: 2023-06-23 18:00:00',
+        'set_time'=>'Enter time like following example: 2023-06-23 18:00:00',
+        'set_second_time'=>'Set second time like following example: 2023-06-23 18:00:00'
+    );
+
     const LOCAL_TOKEN = '6140959186:AAH9EPi8BFGZt6iklYy-BdlSmpvHheJ0h6I/';
     const BASIC_URL = 'https://api.telegram.org/bot';
     protected $chat_id;
 
-
-
-    public function __construct($chat_id){
+    public function __construct(int $chat_id)
+    {
         $this->chat_id = $chat_id;
     }
 
-    public function sendMessage($message){
+    public function setChatId(int $chat_id):self{$this->chat_id = $chat_id; return $this;}
+    public function getChatId():int{return $this->chat_id;}
+
+    public function sendMessage(string $message):void
+    {
         $data = ['chat_id'=>$this->chat_id, 'text'=>$message];
         $ch = curl_init(self::BASIC_URL . self::LOCAL_TOKEN . 'sendMessage?' . http_build_query($data));
         $a = curl_exec($ch);
         curl_close($ch);
     }
 
-    public function sendKeyboard($message, $keyboard){
+    public function sendKeyboard(string $message, array $keyboard):void
+    {
 
         $data = ['chat_id'=>$this->chat_id, 'text'=>$message, 'reply_markup'=>json_encode($keyboard)];
         $ch = curl_init(self::BASIC_URL . self::LOCAL_TOKEN . 'sendMessage?' . http_build_query($data));
         $a = curl_exec($ch);
         curl_close($ch);
     }
-    
-    const DICTIONARY = array(
-            'not_listening'=>'',
-            'set_note'=>'Enter your note message',
-            'delete_note'=>'Enter note id to delete it',
-            'delete_remind'=>'Enter note id to delete its remind',
-            'set_remind'=>'Enter note id to edit',
-            'search_note'=>'Enter string to search among notes',
-            'set_between'=>'Set first time like following example: 2023-06-23 18:00:00',
-            //
-            'set_time'=>'Enter time like following example: 2023-06-23 18:00:00',
-            'set_second_time'=>'Set second time like following example: 2023-06-23 18:00:00'
-        );
-        
-    public function setState($keyword){
+     
+    public function setState(string $keyword):void
+    {
         $_SESSION['state'] = $keyword;
         session_write_close();
         $this->sendMessage(self::DICTIONARY[$keyword]);
     }
-
 }
